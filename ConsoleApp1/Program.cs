8272519329739b9b30fef02e461b4ca620f4d086
Admin <Admin@DESKTOP-SUH30OM>
2024-11-18 21:09:51 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using Npgsql;
+using System;
+
+
+class Wamari
+{
+    static void Main(string[] args)
+    {
+        String ConnString = ("Server=localhost; Database=lamisplus; Password=lamis; Port=5432; User ID=postgres;");
+		try
+		{
+			using (var conn = new NpgsqlConnection(ConnString)) 
+				conn.Open();
+			Console.WriteLine("Connected Successfully");
+		}
+		catch (Exception ErrorMessage)
+		{
+
+			Console.WriteLine($"An error occured;{ErrorMessage}");
+		}
+
+    }
+}
